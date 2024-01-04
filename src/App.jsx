ec482c484f1a78f0761f41b997a05d2b715376fb
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-01-04 14:43:13 +0700

    - init modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { useSelector, useDispatch } from 'react-redux'
 import CartContainer from './components/CartContainer'
 import Navbar from './components/Navbar'
+import Modal from './components/Modal'
 import { useEffect } from 'react'
 import { calculateTotal } from './features/cart/cartSlice'
+
 function App() {
   const { cartItems } = useSelector((state) => state.cart)
   const dispatch = useDispatch()
+
   useEffect(() => {
     dispatch(calculateTotal())
   }, [cartItems])
+
   return (
     <main>
       <Navbar />
+      <Modal />
       <CartContainer />
     </main>
   )
